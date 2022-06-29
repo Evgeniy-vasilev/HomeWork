6ce7250a1829ce8ad34d26932d4e00863fc02e8c
Evgeniy <e.vasiliev.86@mail.ru>
2022-06-29 09:26:21 +0300

    Homework

---
+++ task1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int a = int.Parse(Console.ReadLine());
+    return a;
+}
+
+int num = Prompt("Введите пятизначное число: ");
+int inv = 0;
+int num1 = num;
+
+while (num1 > 0)
+{
+    inv = inv * 10 + num1 % 10;
+    num1 = num1 / 10;
+}
+if (num < 10000 || num > 99999)
+{
+    System.Console.WriteLine("Введено неверное число");
+}
+else if (num == inv)
+{
+    System.Console.WriteLine($"Число {num} является полиндромом");
+}
+else
+{
+    System.Console.WriteLine($"Число {num} не является полиндромом");
+}
---
+++ task_3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным(делится ли оно на два без остатка).
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num >= 0)
+{
+    if (num % 2 == 1)
+    {
+        Console.WriteLine($"{num} не является четным числом");
+    }
+    else
+    {
+        Console.WriteLine($"{num} является четным числом");
+    }
+}
+else
+{
+    if (num % 2 == -1)
+    {
+        Console.WriteLine($"{num} не является четным числом");
+    }
+    else
+    {
+        Console.WriteLine($"{num} является четным числом");
+    }
+}

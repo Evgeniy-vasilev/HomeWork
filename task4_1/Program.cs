a20cde5b7ce746c481b9bd03f74200b726e8723c
Evgeniy <e.vasiliev.86@mail.ru>
2022-07-02 19:45:59 +0300

    HomeWork Seminar 4

---
+++ task4_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Exp(int A, int B)
+{
+    int result = 1;
+    for (int i = 1; i <= B; i++)
+    {
+        result = result * A;
+    }
+    return result;
+}
+
+System.Console.Write("Введите число А: ");
+int A = int.Parse(Console.ReadLine());
+System.Console.Write("Введите число B: ");
+int B = int.Parse(Console.ReadLine());
+int exp = Exp(A, B);
+System.Console.WriteLine($"Результат: {exp}");
---
+++ task4_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int a = int.Parse(Console.ReadLine());
+    return a;
+}
+
+int num = Prompt("Введите число: ");
+int sum = 0;
+
+while (num > 0)
+{
+    sum += num % 10;
+    num = num / 10;
+}
+
+System.Console.WriteLine($"Сумма цифр равна: {sum}");
